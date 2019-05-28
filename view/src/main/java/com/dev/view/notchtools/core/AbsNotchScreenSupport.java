7110ce60e7ca4072ae82b06304146cad93cf92f8
storm <storm@gmail.com>
2019-05-28 21:52:03 +0800

    添加处理刘海屏的库

---
+++ view/src/main/java/com/dev/view/notchtools/core/AbsNotchScreenSupport.java
@@ -0,0 +1,64 @@
+package com.dev.view.notchtools.core;
+
+import android.app.Activity;
+import android.view.Window;
+import com.dev.view.notchtools.helper.NotchStatusBarUtils;
+
+
+/**
+ * @author zhangzhun
+ * @date 2018/11/4
+ */
+
+public abstract class AbsNotchScreenSupport implements INotchSupport {
+
+    @Override
+    public int getStatusHeight(Window window) {
+        return NotchStatusBarUtils.getStatusBarHeight(window.getContext());
+    }
+
+    @Override
+    public void fullScreenDontUseStatus(Activity activity, OnNotchCallBack notchCallBack) {
+        NotchStatusBarUtils.setFullScreenWithSystemUi(activity.getWindow(), false);
+        onBindCallBackWithNotchProperty(activity, notchCallBack);
+    }
+
+    @Override
+    public void fullScreenDontUseStatusForPortrait(Activity activity, OnNotchCallBack notchCallBack) {
+        fullScreenDontUseStatus(activity, notchCallBack);
+    }
+
+    @Override
+    public void fullScreenDontUseStatusForLandscape(Activity activity, OnNotchCallBack notchCallBack) {
+        fullScreenDontUseStatus(activity, notchCallBack);
+    }
+
+    @Override
+    public void fullScreenUseStatus(Activity activity, OnNotchCallBack notchCallBack) {
+        NotchStatusBarUtils.setFullScreenWithSystemUi(activity.getWindow(), false);
+        onBindCallBackWithNotchProperty(activity, getNotchHeight(activity.getWindow()), notchCallBack);
+    }
+
+    protected void onBindCallBackWithNotchProperty(Activity activity, OnNotchCallBack notchCallBack) {
+        if (notchCallBack != null) {
+            NotchProperty notchProperty = new NotchProperty();
+            notchProperty.setNotchHeight(getNotchHeight(activity.getWindow()));
+            notchProperty.setNotch(isNotchScreen(activity.getWindow()));
+            if (notchCallBack != null) {
+                notchCallBack.onNotchPropertyCallback(notchProperty);
+            }
+        }
+    }
+
+    protected void onBindCallBackWithNotchProperty(Activity activity, int marginTop, OnNotchCallBack notchCallBack) {
+        if (notchCallBack != null) {
+            NotchProperty notchProperty = new NotchProperty();
+            notchProperty.setNotchHeight(getNotchHeight(activity.getWindow()));
+            notchProperty.setNotch(isNotchScreen(activity.getWindow()));
+            notchProperty.setMarginTop(marginTop);
+            if (notchCallBack != null) {
+                notchCallBack.onNotchPropertyCallback(notchProperty);
+            }
+        }
+    }
+}
